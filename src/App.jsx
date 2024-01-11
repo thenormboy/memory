0aa1210a46ba696f2d9b3ebcc10293386453e65b
Bleaze Perris <bperriscode@gmail.com>
2024-01-11 10:09:16 -0500

    clicking a component now randomizes the components

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.css'
 import Header from './Header'
 import Game from './Game'
 
+let weaponInfo = ['1', '90', '170', '262', '345', '430', '515', '600', '698', '756', '832', '912', "997", '1076']
+
 function App() {
 
   return (
     <>
       <Header />
-      <Game />
+      <Game weaponTypes={weaponInfo}/>
     </>
   )
 }
--- src/Game.jsx
+++ src/Game.jsx
@@ -1,16 +1,30 @@
 import { useEffect } from "react";
 import { useState } from "react";
 
-export default function Game() {
+export default function Game({weaponTypes}) {
 
     shuffleArray(weaponTypes)
+
+    const [elements, setElements] = useState(weaponTypes)
+    const [count, setCount] = useState(0)
+
+    function handleClick() {
+        let changedArray = shuffleArray(elements)
+        //console.log(changedArray)
+        setCount(count + 1)
+        setElements([...changedArray])
+    }
+
+    console.log(count)
 
     return (
         <>
         <div className="memory-container">
-        {weaponTypes.map((info) => {
+        {elements.map((info, index) => {
             return (
-                <MemoryComponent imgData={info}/>
+                <button className="component" onClick={() => handleClick()}>
+                    <MemoryComponent key={Date.now()} imgData={info}/>
+                </button>
             )
         })}
         </div>
@@ -18,19 +32,20 @@ export default function Game() {
     )
 }
 
-let weaponTypes = ['1', '90', '170', '262', '345', '430', '515', '600', '698', '756', '832', '912', "997", '1076']
-
 function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]]
     }
+    return (array)
 }
 
 
 function MemoryComponent({imgData}) {
 
     const [data, setData] = useState(null);
+
+    //console.log(imgData)
 
     useEffect(() => {
         fetch('https://mhw-db.com/weapons/' + imgData)
@@ -40,8 +55,6 @@ function MemoryComponent({imgData}) {
     }, []);
 
     return (
-        <div className="component">
-            <img src={data} />
-        </div>
+        <img key={Date.now()} src={data} />
     )
 }
